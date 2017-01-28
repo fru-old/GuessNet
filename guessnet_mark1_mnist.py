be9554e28c44316e18c9a17908086a84a1e78ffe
Florian Rüberg <florian.rueberg@gmail.com>
2017-01-28 12:28:28 +0100

    Cleanup

--- guessnet_mark1_mnist.py
+++ guessnet_mark1_mnist.py
@@ -8,6 +8,7 @@ from guessnet import GuessNet
 import random
 import math
 import array
+import time
 
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
@@ -15,8 +16,8 @@ mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 IMAGE_SIZE = 28
 IMAGE_PIXELS = IMAGE_SIZE * IMAGE_SIZE
 
-ONE_HOT_RANDOM_SIZE = 3
-GUESS_COUNT_PER_INPUT = 5
+ONE_HOT_RANDOM_SIZE = 30
+GUESS_COUNT_PER_INPUT = 20
 BATCH_SIZE = 1
 
 
@@ -28,13 +29,19 @@ class GuessNetMark1Mnist:
     def layers(self):
         compressed = tf.reshape(self.one_hot_random, shape=[BATCH_SIZE * GUESS_COUNT_PER_INPUT, ONE_HOT_RANDOM_SIZE])
         guesses_compressed = self.__guess(compressed)
+        tf.summary.image('guesses', tf.reshape(guesses_compressed, [BATCH_SIZE*GUESS_COUNT_PER_INPUT, IMAGE_SIZE, IMAGE_SIZE, 1]), max_outputs=40)
         guesses = tf.reshape(guesses_compressed, shape=[BATCH_SIZE, GUESS_COUNT_PER_INPUT, IMAGE_PIXELS])
         return self.__generate(guesses, self.__guess_mask(self.__error(guesses)))
 
     def __guess(self, one_hot_random_compressed):
-        W1 = tf.Variable(tf.truncated_normal([ONE_HOT_RANDOM_SIZE, IMAGE_PIXELS], stddev=0.1))
-        b1 = tf.Variable(tf.constant(0.1, shape=[IMAGE_PIXELS]))
-        return tf.nn.relu(tf.matmul(one_hot_random_compressed, W1) + b1)
+        W1 = tf.Variable(tf.truncated_normal([ONE_HOT_RANDOM_SIZE, 1024], stddev=0.1))
+        b1 = tf.Variable(tf.constant(0.1, shape=[1024]))
+        r1 = tf.nn.relu(tf.matmul(one_hot_random_compressed, W1) + b1)
+
+        W2 = tf.Variable(tf.truncated_normal([1024, IMAGE_PIXELS], stddev=0.1))
+        b2 = tf.Variable(tf.constant(0.1, shape=[IMAGE_PIXELS]))
+        r2 = tf.nn.sigmoid(tf.matmul(r1, W2) + b2)
+        return r2
 
     def __error(self, guesses):
         original_x_guess_count = tf.tile(tf.expand_dims(self.original, 0), [1, GUESS_COUNT_PER_INPUT, 1])
@@ -42,8 +49,7 @@ class GuessNetMark1Mnist:
 
     def __guess_mask(self, error):
         maximum_error = tf.expand_dims(tf.reduce_max(error, axis=1), 1)
-        #tf.matmul(maximum_error, error)
-        grounded_error = tf.exp(tf.exp(maximum_error - error) - 1) - 1
+        grounded_error = maximum_error - error
         scale = tf.expand_dims(tf.reduce_sum(grounded_error, axis=1), 1)
         return grounded_error / scale
 
@@ -59,7 +65,7 @@ cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=ne
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 tf.summary.scalar('cross_entropy', cross_entropy)
-tf.summary.image('guess', tf.reshape(layers, [BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, 1]))
+tf.summary.image('result', tf.reshape(layers, [BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, 1]))
 merged = tf.summary.merge_all()
 
 init = tf.global_variables_initializer()
@@ -70,7 +76,7 @@ with tf.Session() as sess:
     writer = tf.summary.FileWriter('/code/logs', graph=tf.get_default_graph())
 
     # Train
-    for i in range(10000):
+    for i in range(100000):
         batch_original, _ = mnist.train.next_batch(BATCH_SIZE)
         feed_dict = {
             net.one_hot_random: GuessNet.get_one_hot_random(shape=[BATCH_SIZE, GUESS_COUNT_PER_INPUT, ONE_HOT_RANDOM_SIZE]),
@@ -82,65 +88,4 @@ with tf.Session() as sess:
         writer.add_summary(summary, i)
         writer.flush()
         print(error)
-
-
-"""
-def layer(batch_size):
-    guess = []
-    error = []
-    for i in range(GUESS_COUNT_PER_INPUT):
-        guess.append(cell(tf_get_element_at(one_hot_random, i, [batch_size, ONE_HOT_RANDOM_SIZE])))
-        error.append(tf.reduce_mean(500 - tf.abs(guess[i] - original), 1))
-
-    #transposed_error = tf.transpose(tf.pack(error))
-    #normalized_error = tf.reduce_max(transposed_error, axis=1) - transposed_error# - tf.reduce_min(transposed_error, axis=1)
-    #normalized_error = 1 - tf.truediv((transposed_error - tf.reduce_min(transposed_error, axis=1)), tf.reduce_max(transposed_error, axis=1))
-
-    normalized_error = tf.transpose(tf.pack(error)) #tf.nn.softmax(pack, dim=0)
-    guess_reshaped = tf.transpose(guess, perm=[1,2,0])
-    error_reshaped = tf.expand_dims(normalized_error, 2)
-
-    return tf.squeeze(tf.matmul(guess_reshaped, error_reshaped), axis=2)
-
-
-
-    #return tf.reduce_sum(guess_sum, axis=0)
-    #softmax_error = tf.nn.softmax(tf.pack(error), dim=0)
-    #return tf.squeeze(tf.matmul(tf.expand_dims(softmax_error, 1), guess), axis=1)
-
-    #error_reshaped = tf.expand_dims(tf.transpose(softmax_error),1)
-    #guess_reshaped = tf.transpose(guess, perm=[1,0,2])
-
-    #return tf.squeeze(tf.matmul(error_reshaped, guess_reshaped), axis=1)
-    #result before softmax [batch, GUESS_COUNT_PER_INPUT]
-
-
-layer1 = layer(BATCH_SIZE)
-
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=original, logits=layer1))
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-
-
-
-tf.summary.scalar('cross_entropy', cross_entropy)
-img = tf.reshape(layer1, [BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, 1])
-tf.summary.image('guess', img)
-merged = tf.summary.merge_all()
-
-with tf.Session() as sess:
-    sess.run(init)
-
-    writer = tf.summary.FileWriter('/code/logs', graph=tf.get_default_graph())
-
-    # Train
-    for i in range(340):
-        batch_original, _ = mnist.train.next_batch(BATCH_SIZE)
-        feed_dict = {one_hot_random: get_one_hot_random_batch(BATCH_SIZE), original: batch_original}
-        summary, _ = sess.run([merged, train_step], feed_dict=feed_dict)
-        error = sess.run(cross_entropy, feed_dict=feed_dict)
-        writer.add_summary(summary, i)
-        writer.flush()
-        print(error)
-
-    writer.close()
-"""
+        time.sleep(1*60)
